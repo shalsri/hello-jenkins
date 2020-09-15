2fe864ecbc8d90390e510c2dc2e00dae6eaa2a95
sh sri <shalinisridhar@outlook.com>
2020-09-15 12:28:11 +0530

    Add get route changes

--- test/test.js
+++ test/test.js
@@ -4,11 +4,11 @@ var app = require('../app.js');
  
  
  
-describe('GET /testroute', function() {
+describe('GET /', function() {
  
-  it('respond with hello world of jenkins', function(done) {
+  it('respond with hello world of yet again', function(done) {
  
-    request(app).get('/testroute').expect('hello world of jenkins again', done);
+    request(app).get('/').expect('hello world of jenkins yet again', done);
  
   });
  
